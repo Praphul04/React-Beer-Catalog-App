c4cbe470eed3c6d2707e14da98f549f6ca7a7292
Praphul04 <praphulthakur6@gmail.com>
2024-03-09 23:16:55 +0530

    Beer-App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [beers, setBeers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [beersPerPage] = useState(12);
+
+  useEffect(() => {
+    axios.get('https://api.punkapi.com/v2/beers')
+      .then(response => {
+        setBeers(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+      });
+  }, []);
+
+  const indexOfLastBeer = currentPage * beersPerPage;
+  const indexOfFirstBeer = indexOfLastBeer - beersPerPage;
+  const currentBeers = beers.slice(indexOfFirstBeer, indexOfLastBeer);
+
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  const filteredBeers = currentBeers.filter(beer =>
+    beer.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Beer Catalog</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Search beers"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="beer-box">
+        <div className="beer-container">
+          {filteredBeers.map(beer => (
+            <div key={beer.id} className="beer-card">
+              <img src={beer.image_url} alt={beer.name} />
+              <h2>{beer.name}</h2>
+              <p>{beer.tagline}</p>
+            </div>
+          ))}
+        </div>
+        <div className="pagination">
+          {Array.from({ length: Math.ceil(beers.length / beersPerPage) }).map((_, index) => (
+            <button key={index} onClick={() => paginate(index + 1)}>
+              {index + 1}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
